9bdf9fcc4a112056dafdb4389c00f8ecc6af5f5f
ChaseWright294 <chasew454@gmail.com>
2026-03-26 19:33:39 -0500

    Database configuerd and tables created

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ const express = require('express'); //using express for server
 const { generateKeyPair } = require("./keys");
 const { jwksManager, getKeyByKid } = require("./jwks");
 const { authManager } = require("./auth");
+const sqlite3 = require('sqlite3').verbose(); //using sqlite for database
 
 const app = express();
 app.use(express.json());
 
 const port = 8080;
+
+let db = new sqlite3.Database('./totally_not_my_privateKeys.db', (err) =>
+{
+    if (err) {
+        console.error('Error opening database:', err.message);
+    }
+    console.log('Connected to the SQLite database.');
+})
+
+db.run(`CREATE TABLE IF NOT EXISTS keys (
+    kid INTEGER PRIMARY KEY AUTOINCREMENT,
+    key BLOB NOT NULL,
+    expires_at INTEGER NOT NULL
+    )`);
 
 //generate some keys on server startup, with some expired and some not
 for (let i = 0; i < 15; i++) {
